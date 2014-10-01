b804abdad8b5d4402724712815ce654ead0a4e5e
= <=>
2014-10-01 18:56:45 -0500

    filled in some calls in main, also added functions files

--- generate.php
+++ generate.php
@@ -2,4 +2,14 @@
 
 	// This is the main file which will generate the test images
 
+	require 'parseConfig.php';
+	require 'discoverDirs.php';
+
+	$CONFIGFILE = "config.txt";
+
+	$config = parseConfig($CONFIGFILE);
+
+	$config["targets"] = discoverTargets($config["targetsDir"]);
+	$config["backgrounds"] = discoverBackgrounds($config["backgroundsDir"]);
+
 ?>
