ab7ad61393d457f1092df3ad38805cdbcb92593d
= <=>
2014-10-01 23:12:58 -0500

    changed woods4 back, and tryied to fix generate

--- generate.php
+++ generate.php
@@ -2,14 +2,20 @@
 
 	// This is the main file which will generate the test images
 
-	require 'parseConfig.php';
-	require 'discoverDirs.php';
+	require 'parse_config.php';
+	require 'discover_dirs.php';
+	require 'write_json.php';
 
-	$CONFIG_FILE = "config.txt";
+	$CONFIG_FILE = "config.json";
+	$OUTPUT_FILE = "output.json";
 
 	$config = parse_config($CONFIG_FILE);
 
 	$config["targets"] = discover_dirs($config["targetsDir"]);
 	$config["backgrounds"] = discover_dirs($config["backgroundsDir"]);
+	print($config["backgroundsDir"]);
+
+	// loop through the combinations of options and call the image generation for each combination
+	
 
 ?>
