02e86ef1743286fe875a37a0ffe836c1fa2dd7ee
= <=>
2014-10-01 19:55:44 -0500

    typo fix

--- generate.php
+++ generate.php
@@ -7,7 +7,7 @@
 
 	$CONFIG_FILE = "config.txt";
 
-	$config = parse_config($CONFIGFILE);
+	$config = parse_config($CONFIG_FILE);
 
 	$config["targets"] = discover_dirs($config["targetsDir"]);
 	$config["backgrounds"] = discover_dirs($config["backgroundsDir"]);
