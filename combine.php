8dd5e55fd63d4690ef7fa74e3ef95351a6b36d6b
= <=>
2014-10-08 19:22:55 -0500

    combine function works now

--- combine.php
+++ combine.php
@@ -1,95 +1,76 @@
 <?php
 
-	/*function combine($curr, $remaining) {
+	function combine( $remaining ) {
 
-		if(empty($remaining))
-			return;
+		// if there are no remaining 
+		if ( empty( $remaining ) ) {
+			print "No items in remaining array \n";
+			return array();
+		}
 
-		// if curr is empty, push a new empty array onto it
-		if(empty($curr))
-			array_push($curr, array());
+		// if there are remaining values
+		// pop the top key and value off of the $remaining array
+		$value = reset( $remaining );
+		$key = key( $remaining );
+		unset($remaining[$key]);
 
-		$next = array_pop($remaining);
+		// call combine() again and operate on it's output
+		$result_array = combine( $remaining );
 
-		foreach($curr as $curr_key => $curr_value) {
-			echo "curr key: " . $curr_key . " and curr value: " . $curr_value . "\n";
-			foreach ($next as $next_key => $next_value) {
-				echo "next key: " . $next_key . " and next value: " . $next_value . "\n";
-				if(this_key == 0) {
-					array_push($curr_value, $next_value);
-				} else {
-					// clone the current row
-					$new_row = $curr_value;
-					array_push($new_row, $next_value);
-					array_push($curr, $new_row);
+			if( empty( $result_array ) ) {
+				print "Result array was empty, add new items to it \n";
+				// if the result array was empty, then push a new assoc array for each item in $value 
+				for ( $i = 0; $i < count($value); $i++ ) {
+					array_push( $result_array, array(
+							$key => $value[$i]
+						) );
 				}
-				
+			} else {
+				print "Call to combine with result array not empty\n";
+				// otherwise, iterate from the bottom of $result_array up to the top
+				for ( $i = (count( $result_array ) - 1); $i >= 0; $i-- ) {
+					//$handle = fopen ("php://stdin","r");
+					//$line = fgets($handle);
+					print "Index " . $i . " of result array\n";
+					// get the row at that index and add the first value to it
+					$row = $result_array[$i];
+					$row[$key] = $value[0];
+					// then reinsert it back into the array at the original index
+					$result_array[$i] = $row;
+					// for the remaining items in $value, add them to row and then push those onto the array
+					for ( $j = 1; $j < count($value); $j++ ) {
+						$row[$key] = $value[$j];
+						array_push( $result_array, $row );
+					}
+				}
 			}
-			$curr = combine($curr, $remaining);
-		}
 
-		return $curr;
+			return $result_array;
 
 	}
 
-	$test_array = array(
+	/*
 
-		'sizes' => array(1, 2, 3),
-		'backgrounds' => array('one', 'two', 'three'),
-		'filters' => array('none', 'blur')//,
-		//'single' => 999
-
-	);
-
-	$result = array();
-
-	$result = combine($result, $test_array);
-
-	var_dump($result);*/
-
-	function combos($data, $all = array(), $group = array(), $val = null, $i = 0)
-	{
-		if (isset($val))
-		{
-			array_push($group, $val);
-		}
-	 
-		if ($i >= count($data))
-		{
-			array_push($all, $group);
-		}
-		else
-		{
-			foreach ($data as $v)
-			{
-				combos($data, &$all, $group, $v, $i + 1);
-			}
-		}
-	 
-		return $all;
-	}
-	 
 	$data = array
 	(
-		array('a', 'b'),
-		array('e', 'f', 'g'),
-		array('w', 'x', 'y', 'z'),
+		"key1" => array('a', 'b'),
+		"key2" => array('e', 'f', 'g')
 	);
 
 	$test_array = array(
-
 		'sizes' => array(1, 2, 3),
 		'backgrounds' => array('one', 'two', 'three'),
-		'filters' => array('none', 'blur')//,
-		//'single' => 999
-
+		'filters' => array('none', 'blur'),
+		'single' => array( 999 )
 	);
 	 
 	//$combos1 = combos($data);
-	$combos2 = combos($test_array);
+	$combos2 = combine($test_array);
 	 
 	//print_r($combos1);
 	var_dump($combos2);
+
+	*/
 
 
 ?>
